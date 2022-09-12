e3e0d6a5ac256b07e95724f26347767659469036
Fernando Carvalho <fernandocarvalho2304@gmail.com>
2022-09-12 20:57:26 -0300

    Commit Conta e ContaCorrente
    Conta:
      métodos criados:
        sacar, depositar e transferir.
      declarados mais atributos.
    Conta Corrente:
      criado método:
        relatorioTributação.

--- banco_grupo5/src/conta/Conta.java
+++ banco_grupo5/src/conta/Conta.java
@@ -5,17 +5,60 @@ public abstract class Conta {
 	private double saldo;
 	private String idAgencia;
 	private final String TIPO = "CONTA";
-	
+	public final double TARIFA = 0.10;
+	public final double TRANSFERIRTARIFA = 0.20;
+	public int contadorSaque = 0;
+	public int contadorDeposito = 0;
+	public int contadorTransferencia= 0;
 
 	public Conta() {
 	}
+	
 	public Conta(String cpfTitular, double saldo, String idAgencia) {
 		this.cpfTitular = cpfTitular;
 		this.saldo = saldo;
 		this.idAgencia = idAgencia;
-		
-	
 	}
+	
+	
+	public void sacar(double valor) {
+        if (this.saldo > valor + TARIFA) {
+            double novoSaldo = this.saldo - valor - TARIFA;
+            this.saldo = novoSaldo;
+            this.contadorSaque++;
+        } else {
+            System.out.println("Saque rejeitado!\nSaldo insuficiente para saque.");;
+        }
+    }
+	//criar erro proposital.
+	public void depositar(double deposito) {
+       if(deposito > TARIFA) {
+    	   this.saldo = this.saldo + deposito - TARIFA; 
+    	   this.contadorDeposito++;
+       }
+       else {
+    	   System.out.println("Deposito rejeitado!\nValor mínimo nescessário R$ 0,11.");
+       }
+    }
+       
+	
+	public void transferir(Conta destino, double valor) {
+        if (this.saldo >= valor + TRANSFERIRTARIFA) {
+            this.saldo = this.saldo - valor;
+            destino.saldo = destino.saldo + valor;
+            this.contadorTransferencia++;
+        } else {
+            System.out.println("N�o h� saldo suficiente para transfer�ncia");
+        }
+    }
+	
+	
+	
+	
+	
+	
+	
+	
 	public String getCpfTitular() {
 		return cpfTitular;
 	}
--- banco_grupo5/src/conta/ContaCorrente.java
+++ banco_grupo5/src/conta/ContaCorrente.java
@@ -4,8 +4,24 @@ package conta;
 public class ContaCorrente extends Conta{
 	private final String TIPO = "CONTACORRENTE";
 	
-	public ContaCorrente() {		
+	public ContaCorrente() {				
 	}
+
+	public ContaCorrente(String cpfTitular, double saldo, String idAgencia) {
+		super(cpfTitular, saldo, idAgencia);
+	}
+	public void relatorioTributacao() {		
+		System.out.printf("\nTributação:\nTotal Saques: R$%.2f%n"+contadorSaque * TARIFA +
+				"\nValor únitario da tarifa: R$%.2f%n"+TARIFA+
+				"\nTotal Depósitos: R$%.2f%n"+contadorDeposito * TARIFA+
+				"\nValor únitario da tarifa: R$%.2f%n"+TARIFA+
+				"\nTotal Transferências: R$%.2f%n"+contadorTransferencia * TRANSFERIRTARIFA +
+				"\nValor únitario da tarifa: R$%.2f%n"+TRANSFERIRTARIFA);
+		System.out.printf("\nValor total gasto: R$%.2f%n"+ ((contadorSaque * TARIFA)+(contadorDeposito * TARIFA)+(contadorTransferencia * TRANSFERIRTARIFA)));
+			
+	}
+	
+	
 	
 	
 }

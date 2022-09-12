46435cb8f9ca2e5b50f31f71d015aadca52b7f72
GiseleBr <britto.gisele@gmail.com>
2022-09-12 18:54:00 -0300

    Mudança Classe Diretor
    adicionado os construtores

--- banco_grupo5/src/funcionario/Diretor.java
+++ banco_grupo5/src/funcionario/Diretor.java
@@ -1,5 +1,24 @@
 package funcionario;
 
-public class Diretor extends Funcionario{
+public class Diretor extends Funcionario {
+	
+	public String CARGO = "DIRETOR";
+		
+		public Diretor() {
+			super();
+		}
+	
+		public Diretor(String cpf, String senha) {
+			super(cpf, senha);
+		}
+	
+	public void relatorioInformacoesCliente() {
+		
+	}
+		
+		
+		
+	/*Relatório com as informações de Nome, CPF e Agência de todos os
+		clientes do sistema em ordem alfabética*/
 
 }

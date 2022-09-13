9a46548af65e538239348ff13c16abfef7491868
jonnyband <jmsupernet1@gmail.com>
2022-09-13 16:56:00 -0300

    Classe Menu
    Criada Classe Menu
    Criados os metodos login, menuCliente e inicio das listas.

--- banco_grupo5/src/conta/ContaPoupanca.java
+++ banco_grupo5/src/conta/ContaPoupanca.java
@@ -1,22 +1,29 @@
 package conta;
 
-public class ContaPoupanca extends Conta{
-	private final String TIPO = "CONTAPOUPANCA";
-	
-	public ContaPoupanca() {
-		
-	}
-	
-	public ContaPoupanca(String cpfTitular, double saldo, String idAgencia) {
-		super(cpfTitular, saldo, idAgencia);
-		
-	}
-	
-	public double relatorioRendimento() {
-		return 0;
-		
-	}
-		
-	
-}
+import java.text.DecimalFormat;
 
+public class ContaPoupanca extends Conta{
+    private final String TIPO = "CONTAPOUPANCA";
+    private final double PORCENTAGEMRENDIMENTO = 0.0165;
+
+
+    public ContaPoupanca() {
+
+    }
+
+    public ContaPoupanca(String cpfTitular, double saldo, String idAgencia) {
+        super(cpfTitular, saldo, idAgencia);
+
+    }
+
+    public void relatorioRendimento(double valor, int quantDias) {
+		DecimalFormat df = new DecimalFormat();
+		df.setMaximumFractionDigits(2);
+		double valor1 = valor + (valor * (quantDias * (PORCENTAGEMRENDIMENTO/100))); 
+        System.out.println("Valor final será de: R$"+ df.format(valor1));
+
+        }
+
+
+
+}
